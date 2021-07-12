a1c6e819d7dfca568c19bc41246777e82cd1a4c5
daniyal8657 <87060972+daniyal8657@users.noreply.github.com>
2021-07-12 01:09:59 -0700

    Adding

---
+++ add.js
@@ -0,0 +1,9 @@
+function addMe(num1, num2){
+    var sum= num1+ num2;
+    // console.log("Sum of " + num1, " and "  + num2 + " is equal to " + sum);
+    // Litrals ``
+    console.log(`Sum of ${num1} and ${num2} is equal to ${sum}`);
+}
+
+addMe(23,46);
+addMe(39,67);
