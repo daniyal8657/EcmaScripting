5a360de029d55e9d9b4aa326ae6838bb1b8617d1
daniyal8657 <87060972+daniyal8657@users.noreply.github.com>
2021-07-18 11:27:40 +0530

    Fencing Problem

---
+++ FencingForAny.js
@@ -0,0 +1,10 @@
+const FencingForAny = ( Edges, NumOfWire, Rate) => {
+    let sum=0;
+    for (let i=0; i< Edges.length; i++) {
+    return sum+Edges[i];
+}
+return sum * NumOfWire * Rate;
+};
+
+console.log(FencingForAny{34,24,53,75}, 12 ,10);
+
